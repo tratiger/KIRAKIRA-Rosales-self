ddc8eee1cf7b3db6d87dd60271c054ed31eff603
YasitoriOvO <caijidayo@outlook.com>
2025-03-06 23:13:30 +0800

    ✨ 更新邮件样式

--- src/common/i18n.ts
+++ src/common/i18n.ts
@@ -1,12 +1,13 @@
 import English from "../locales/English.js";
 import ChineseSimplified from "../locales/Chinese Simplified.js";
+import EmailTemplate from "./EmailTemplate.js";
 
 const languagePacks: Record<string, any> = {
 	"zh-Hans-CN": ChineseSimplified,
 	"en-US": English,
 	// "fr-FR": French,
 	// "ja-JP": Japanese
-};
+}
 
 /**
  * 判断客户端的语言并返回对应的语言包
@@ -16,5 +17,17 @@ const languagePacks: Record<string, any> = {
  */
 export const geti18nLanguagePack = async (clientLanguage: string, targetMail: string): Promise<any> => {
 	const languagePack = languagePacks[clientLanguage] ?? English;
-	return targetMail in languagePack ? languagePack[targetMail] : null;
+	if (languagePack[targetMail]) {
+		const replacements: Record<string, string> = {
+			"{{mailLine0}}": languagePack[targetMail].mailLine0,
+			"{{mailLine1}}": languagePack[targetMail].mailLine1,
+			"{{mailLine2}}": languagePack[targetMail].mailLine2,
+		};
+
+		const mailHtml = Object.entries(replacements).reduce((html, [key, value]) => html.replace(new RegExp(key, "g"), value),EmailTemplate.mailHtml)
+		const mailTitle = languagePack[targetMail].mailTitle;
+		
+		return { mailTitle: mailTitle, mailHtml: mailHtml };
+	}
+	return null;
 };
--- src/locales/Chinese Simplified.ts
+++ src/locales/Chinese Simplified.ts
@@ -1,22 +1,32 @@
 export default {
   SendVerificationCode: {
     mailTitle: "KIRAKIRA - 注册验证码",
-    mailHtml: `<p>Your registration verification code is: <strong>{{verificationCode}}</strong></p> Welcome to KIRAKIRA. You can use this verification code to register your account. <br> Verification code is valid for 30 minutes. Please ensure do not disclose your verification code to others. <br> <br> To stop receiving notifications, please contact the KIRAKIRA support team.`
+    mailLine0: "你的注册验证码是：",
+    mailLine1: "欢迎来到 KIRAKIRA。你可以使用这个验证码来注册你的账户。",
+    mailLine2: "验证码 30 分钟内有效。请注意安全，不要向他人泄露你的验证码。",
   },
   SendChangeEmailVerificationCode: {
     mailTitle: "KIRAKIRA - 更改邮箱验证码",
-    mailHtml: `<p>你更改邮箱的验证码是：<strong>{{verificationCode}}</strong></p><br>证码 30 分钟内有效。请注意安全，不要向他人泄露你的验证码。`
+    mailLine0: "你更改邮箱的验证码是：",
+    mailLine1: "验证码 30 分钟内有效。请注意安全，不要向他人泄露你的验证码。",
+    mailLine2: "",
   },
   SendChangePasswordVerificationCode: {
     mailTitle: "KIRAKIRA - 更改密码验证码",
-    mailHtml: `<p>你更改密码的验证码是：<strong>{{verificationCode}}</strong></p><br>验证码 30 分钟内有效。请注意安全，不要向他人泄露你的验证码。`
+    mailLine0: "你更改密码的验证码是：",
+    mailLine1: "验证码 30 分钟内有效。请注意安全，不要向他人泄露你的验证码。",
+    mailLine2: "",
   },
   UserEmailAuthenticator: {
     mailTitle: "KIRAKIRA - 验证 2FA 的验证码",
-    mailHtml: `<p>验证 2FA 的验证码是：<strong>{{verificationCode}}</strong></p>注意：你可以使用这个验证码来验证你的 2FA（二重身份验证器）。<br>验证码 30 分钟内有效。请注意安全，不要向他人泄露你的验证码。`
+    mailLine0: "验证 2FA 的验证码是：",
+    mailLine1: "注意：你可以使用这个验证码来验证你的 2FA（二重身份验证器）。",
+    mailLine2: "验证码 30 分钟内有效。请注意安全，不要向他人泄露你的验证码。",
   },
   DeleteUserEmailAuthenticator: {
     mailTitle: "KIRAKIRA - 删除 2FA 的验证码",
-    mailHtml: `<p>删除 2FA 的验证码是：<strong>{{verificationCode}}</strong></p>注意：你可以使用这个验证码来删除你的 2FA（二重身份验证器）。<br>验证码 30 分钟内有效。请注意安全，不要向他人泄露你的验证码。`
+    mailLine0: "删除 2FA 的验证码是：",
+    mailLine1: "注意：你可以使用这个验证码来删除你的 2FA（二重身份验证器）。",
+    mailLine2: "验证码 30 分钟内有效。请注意安全，不要向他人泄露你的验证码。",
   }
 };
